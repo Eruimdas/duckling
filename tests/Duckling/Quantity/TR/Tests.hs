6f969267836cc34a22cb652c42ac00f587c87a29
eruimdas <hasan.kemikk@gmail.com>
2022-01-22 21:56:06 +0300

    Added test file for TR Quantity.

---
+++ tests/Duckling/Quantity/TR/Tests.hs
@@ -0,0 +1,24 @@
+-- Copyright (c) 2016-present, Facebook, Inc.
+-- All rights reserved.
+--
+-- This source code is licensed under the BSD-style license found in the
+-- LICENSE file in the root directory of this source tree.
+
+
+module Duckling.Quantity.TR.Tests
+  ( tests
+  ) where
+
+import Data.String
+import Prelude
+import Test.Tasty
+
+import Duckling.Dimensions.Types
+import Duckling.Quantity.TR.Corpus
+import Duckling.Testing.Asserts
+
+tests :: TestTree
+tests = testGroup "TR Tests"
+  [ makeCorpusTest [Seal Quantity] corpus
+  , makeCorpusTest [Seal Quantity] latentCorpus
+  ]
